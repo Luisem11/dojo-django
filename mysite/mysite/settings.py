bda90fbf69cb2435605f02c8a42badfed55c61d2
explora <luis.monsalve@parqueexplora.org>
2020-09-24 08:37:52 -0500

    Create and register API app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapi.apps.MyapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
